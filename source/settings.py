4a6e55fa1b2249dca6eb693bb4d0b3437a9063e8
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-04-27 12:08:32 +0000

    settings changes

--- source/settings.py
+++ source/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['izudada-blog.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['izudada-blog.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
