0250c831efbdb004981573835282650a2c59fe5e
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-04-27 12:48:24 +0000

    Editted settings.py

--- source/settings.py
+++ source/settings.py
@@ -146,7 +146,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "login"
-
-
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
