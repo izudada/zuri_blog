39159d72caaed4924c60f134f7c7b723bde193e3
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-04-29 10:17:44 +0000

    Changed debug mode to false in settings.py

--- source/settings.py
+++ source/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['izudada-blog.herokuapp.com', '127.0.0.1', 'localhost', '0.0.0.0']
 
